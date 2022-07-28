cad95b6672b088653a2f793c4ed6251ee65f9d0c
Simon Rogers <simon.d.rogers@gmail.com>
2022-07-28 22:43:40 +0100

    tidying

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 from typing import Union
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import requests
 
 logging.basicConfig(level=logging.INFO)
@@ -16,7 +17,8 @@ transport_api_id = os.environ.get('TRANSPORT_API_ID')
 transport_api_key = os.environ.get('TRANSPORT_API_KEY')
 
 
-
+class TrainRequest(BaseModel):
+    message: str
 
 app = FastAPI()
 
@@ -42,22 +44,62 @@ def clean_departures(departure):
         'platform': pl
     }
 
+def format_departure(departure):
+    return f"{departure['time']}, platform {departure['platform']} ({departure['dest']})"
+
 def next_departures(from_station: str, to_station: str, n: int=2):
     response = requests.get(
         f"https://transportapi.com/v3/uk/train/station/{from_station}/live.json?app_id={transport_api_id}&app_key={transport_api_key}&calling_at={to_station}&darwin=false&train_status=passenger"
     )
     output_dict = response.json()
     departures = output_dict['departures']['all']
-    print(n)
+    logging.debug(n)
+    if len(departures) == 0:
+        return "None found"
     n_fetch = min([n, len(departures)])
     dep = [clean_departures(d) for d in departures[:n_fetch]]
-    return dep
+    out_string = f"Next from {from_station} -> {to_station}\n"
+    out_string += '\n'.join([format_departure(d) for d in dep])
+    return out_string
 
 
-@app.get("/next_train_home")
-def next_train_home(source: str="GLC"):
-    return next_departures(source, "MIN")
+@app.post("/train_request")
+def train_request(request_info: TrainRequest):
+    if request_info.message.lower().startswith("next train home"):
+        tokens = request_info.message.split()
+        source = tokens[3].upper()
+        if len(tokens) > 3:
+            n = int(tokens[4])
+        logging.debug("Next train home from %s", source)
+        return next_departures(source, "MIN", n=n)
+    if request_info.message.lower().startswith("next train gla"):
+        logging.debug("Next train to gla")
+        return next_departures("MIN", "GLC")
+    if request_info.message.lower().startswith("next arrival"):
+        to_station = request_info.message.split()[2].upper()
+        from_station = request_info.message.split()[3].upper()
+        return next_arrival(to_station, from_station)
 
-@app.get("/next_train_gla")
-def next_train_gla():
-    return next_departures("MIN", "GLC")
+def next_arrival(to_station: str, from_station: str):
+    logging.info("Next arrival at %s from %s", to_station, from_station)
+    response = requests.get(
+        f"https://transportapi.com/v3/uk/train/station/{to_station}/live.json?app_id={transport_api_id}&app_key={transport_api_key}&called_at={from_station}&darwin=false&train_status=passenger&type=arrival"
+    )
+    output_list = response.json()['arrivals']['all']
+    logging.info("returned %d", len(output_list))
+    if len(output_list) == 0:
+        # try "calling_at" instead
+        response = requests.get(
+            f"https://transportapi.com/v3/uk/train/station/{to_station}/live.json?app_id={transport_api_id}&app_key={transport_api_key}&calling_at={from_station}&darwin=false&train_status=passenger&type=arrival"
+        )
+        output_list = response.json()['arrivals']['all']
+        logging.info("returned %d", len(output_list))
+        if len(output_list) == 0:
+            return "Nothing found"
+    else:
+        output = output_list[0]
+        return_string = f"{to_station}: {output['aimed_arrival_time']} ({output['status']}). Going to {output['destination_name']}."
+        if not output['status'] == 'ON TIME':
+            return_string += f" Expected: {output['expected_arrival_time']}"
+        return return_string
+    
