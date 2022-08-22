1a86930acd293b85a2da2ffd92079f2214a4f4f3
Simon Rogers <simon.d.rogers@gmail.com>
2022-08-22 22:19:10 +0100

    added missing file

---
+++ menus.py
@@ -0,0 +1,69 @@
+MENU = {
+    1: {
+        1: [
+            [
+                "Leek & Potato Soup",
+                "Beef / Veg (ve) Burger with Seasoned Wedges and Crunchy Salad",
+                "Plain Omelette with Seasoned Wedges & Crunchy Salad",
+                "Yoghurt"
+            ]
+        ],
+        2: [
+                "Vegetable Soup",
+                "Crispy Fish Fillet with Chunky Chips and peas",
+                "Quorn Tikka Masala with Rice & Crunchy Salad (ve)",
+                "Chocolate Mousse"
+        ],
+        3: [
+                "Carrot & Coriander Soup",
+                "Mac & Cheese With Garlic Bread & Broccoli",
+                "Baked Potato With Veg Chilli or Baked Beans & Crunchy Salad (ve)",
+                "Jelly"
+        ],
+        4: [
+                "Veg & Rice Soup",
+                "Cottage Pie With Baby Carrots",
+                "Tuna Mayo or Cheese (ve) Sandwich With Crunchy Salad & Coleslaw",
+                "Ice Cream"
+        ],
+        5: [
+                "Vegetable Soup",
+                "Chicken Chunks / Veg Nuggets (ve) Seasoned Wedges Sweetcorn & Coleslaw",
+                "Pizzini with Seasoned Wedges, Sweet corn & Coleslaw",
+                "Apple Crumble & Custard"
+        ]
+    },
+    2: {
+        1: [
+            "Vegetable Soup",
+            "Mild Chicken / Quorn (ve) Curry with Rice & Mixed Peppers",
+            "Salmon Nibbles with Chunky Chips & Peas",
+            "Ice Cream"
+        ],
+        2: [
+            "Leek & Potato Soup",
+            "Chicken Burger with Seasoned Wedges & Crunchy Salad", 
+            "Tomato & Veg Pasta with Garlic Bread & Broccoli (ve)",
+            "Yoghurt"
+        ],
+        3: [
+            "Yellow Split Pea Soup",
+            "Veg Sausage Roll, Beans and Potato Wedges (ve)",
+            "Quorn Fajitas with Potato Wedges & Mixed Peppers (ve)",
+            "Jelly"
+
+        ],
+        4: [
+            "Vegetable Soup",
+            "Turkey Meatball in Gravy with Creamy Mashed Potato & Carrots",
+            "Cheese (ve) / Tuna Mayo Sandwich with Crunchy Salad and Coleslaw",
+            "Fruit Salad with Cream"
+        ], 
+        5: [
+            "Vegetable Soup",
+            "Jumbo Hot Dog with Baby corn & Crunchy Salad",
+            "Baked Potato with Baked Beans(ve) or Cheese & Crunchy Salad",
+            "Oaty Flapjack"
+        ]
+    }
+}
